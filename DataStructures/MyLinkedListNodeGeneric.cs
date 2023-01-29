d3bf5f367b18797763c00944c7532b03e4e26b85
Dmitry Melentev <dmitry.tomahawk@yandex.ru>
2023-01-29 18:40:03 +0500

    MyLinkedList implementation

--- DataStructures/MyLinkedListNodeGeneric.cs
+++ DataStructures/MyLinkedListNodeGeneric.cs
@@ -5,5 +5,8 @@ public class MyLinkedListNode<T>: IMyLinkedListNode<T>
     public T Data { get; }
     public IMyLinkedListNode<T>? Next { get; set; }
 
-    public MyLinkedListNode(T value) {}
+    public MyLinkedListNode(T value)
+    {
+        Data = value;
+    }
 }
