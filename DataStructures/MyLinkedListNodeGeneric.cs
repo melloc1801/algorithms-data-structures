b586304669576dce02454bd2dbd61156f540bda5
Dmitry Melentev <dmitry.tomahawk@yandex.ru>
2023-01-29 18:36:07 +0500

    implement MyLinkedListNode interface with empty methods

---
+++ DataStructures/MyLinkedListNodeGeneric.cs
@@ -0,0 +1,9 @@
+﻿namespace DataStructures;
+
+public class MyLinkedListNode<T>: IMyLinkedListNode<T>
+{
+    public T Data { get; }
+    public IMyLinkedListNode<T>? Next { get; set; }
+
+    public MyLinkedListNode(T value) {}
+}
