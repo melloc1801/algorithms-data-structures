4a5448b78cce5b1cb497922d6defd3eee7bcb6f9
melloc1801 <dmitry.tomahawk@yandex.ru>
2023-05-14 10:26:50 +0500

    add MyLinkedList Prev property

--- DataStructures/MyLinkedListNodeGeneric.cs
+++ DataStructures/MyLinkedListNodeGeneric.cs
@@ -3,6 +3,7 @@
 public class MyLinkedListNode<T>: IMyLinkedListNode<T>
 {
     public T Data { get; }
+    public IMyLinkedListNode<T>? Prev { get; set; }
     public IMyLinkedListNode<T>? Next { get; set; }
 
     public MyLinkedListNode(T value)
